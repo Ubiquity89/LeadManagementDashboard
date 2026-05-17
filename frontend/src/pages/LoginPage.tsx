aa83c34a8f8165474eb9cea83753b6c67db60f91
Ubiquity89 <ubiquity89@gmail.com>
2026-05-17 14:34:57 +0530

    login page updated

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
+import {  useNavigate } from "react-router-dom";
 
 function LoginPage() {
   const navigate = useNavigate();
@@ -105,12 +105,17 @@ function LoginPage() {
           required
         />
       </div>
+      {error && (
+  <p className="text-red-500 text-sm">
+    {error}
+  </p>
+)}
 
       <button
         type="submit"
         className="w-full bg-black hover:bg-gray-800 transition text-white py-3 rounded-xl font-semibold"
       >
-        Login
+        {loading ? "Logging in..." : "Login"}
       </button>
     </form>
 
